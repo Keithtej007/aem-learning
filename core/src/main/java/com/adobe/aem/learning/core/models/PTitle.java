5d3367f0a0d3afa81be994ddda6f7acfb886f2ad
Keithtej007 <86559991+Keithtej007@users.noreply.github.com>
2022-04-20 15:26:30 -0400

    AEM
    AEM

---
+++ core/src/main/java/com/adobe/aem/learning/core/models/PTitle.java
@@ -0,0 +1,12 @@
+package com.adobe.aem.learning.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class)
+public class PTitle {
+
+
+}
